eb39bd3dee40689e7cec45593b246efdc508b0ab
Robert Rothenberg <rrwo@cpan.org>
2018-01-15 23:57:06 +0000

    Bump version

--- lib/Const/Exporter.pm
+++ lib/Const/Exporter.pm
@@ -5,7 +5,7 @@ use v5.10.0;
 use strict;
 use warnings;
 
-use version; our $VERSION = version->declare('v0.2.4');
+use version; our $VERSION = version->declare('v0.2.5');
 
 use Carp;
 use Const::Fast;
